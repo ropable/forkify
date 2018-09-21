16cd38a6394a19871053fc87acbee661e47f885e
Ashley Felton <ashley@ropable.com>
2018-09-21 11:37:55 +0800

    Add limitRecipeTitle function to searchView.

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -18,6 +18,24 @@ export const clearResults = () => {
   elements.searchResList.innerHTML = ""
 }
 
+const limitRecipeTitle = (title, limit=17) => {
+  if (title.length > limit) {
+    const newTitle = []
+    title.split(" ").reduce((accumulator, currentValue) => {
+      // Callback function called for each member of the title array.
+      if (accumulator + currentValue.length <= limit) {
+        // If the accumulator + current word length is <= the limit, add the word
+        // to the newTitle array.
+        newTitle.push(currentValue)
+      }
+      return accumulator + currentValue.length
+    }, 0)
+    // Return the result, joined as a string.
+    return `${newTitle.join(" ")} ...`
+  }
+  return title
+}
+
 const renderRecipe = recipe => {
   // Private function
   const markup = `
@@ -27,7 +45,7 @@ const renderRecipe = recipe => {
             <img src="${recipe.image_url}" alt="${recipe.title}">
         </figure>
         <div class="results__data">
-            <h4 class="results__name">${recipe.title}</h4>
+            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
             <p class="results__author">${recipe.publisher}</p>
         </div>
     </a>
