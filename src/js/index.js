207a312e6d73da8153f2de729ccbb5126b650181
Ashley Felton <ashley@ropable.com>
2018-10-31 15:47:11 +0800

    Added recipeView.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from "./models/Search"
 import Recipe from "./models/Recipe"
 import * as searchView from "./views/searchView"
+import * as recipeView from "./views/recipeView"
 import {elements, renderLoader, clearLoader} from "./views/base"
 
 /* Global state
@@ -66,10 +67,16 @@ elements.searchResPages.addEventListener("click", e => {
 const controlRecipe = async () => {
   // Get the ID from the URL
   const id = window.location.hash.replace("#", "")
-  console.log(id)
 
   if (id) {
     // Prepare UI for changes.
+    recipeView.clearRecipe()
+    renderLoader(elements.recipe)
+
+    // Highlight selected search item.
+    if (state.search) {
+      searchView.highlightSelected(id)
+    }
 
     // Create recipe object.
     state.recipe = new Recipe(id)
@@ -85,7 +92,10 @@ const controlRecipe = async () => {
       state.recipe.calcServings()
 
       // Render recipe.
-      console.log(state.recipe)
+      //console.log(state.recipe)
+      clearLoader()
+      recipeView.renderRecipe(state.recipe)
+
     } catch (error) {
       console.log(error)
       alert("Error processing recipe!")
