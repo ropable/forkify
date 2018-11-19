51abbb868435df9282291309c0f61b2ec1cef4fa
Ashley Felton <ashley@ropable.com>
2018-11-19 15:19:58 +0800

    Handle servings increase/decrease button clicks.

--- src/js/index.js
+++ src/js/index.js
@@ -107,3 +107,20 @@ const controlRecipe = async () => {
 //window.addEventListener("load", controlRecipe)
 // Below is a quick way to add a single function to multiple event listeners:
 ["hashchange", "load"].forEach(event => window.addEventListener(event, controlRecipe))
+
+// Handling recipe button clicks.
+elements.recipe.addEventListener("click", e => {
+  // Match element, or any child of that element:
+  if (e.target.matches(".btn-decrease, .btn-decrease *")) {
+    // Decrease button is clicked.
+    if (state.recipe.servings > 1) {
+      state.recipe.updateServings("dec")
+    recipeView.updateServingsIngredients(state.recipe)
+    }
+  } else if (e.target.matches(".btn-increase, .btn-increase *")) {
+    // Increase button is clicked.
+    state.recipe.updateServings("inc")
+    recipeView.updateServingsIngredients(state.recipe)
+  }
+  //console.log(state.recipe)
+})
